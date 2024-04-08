673c1c53ffd11d908aec8bf8f84b0e41ededa5bc
ravi0dubey <ravi0dubey@gmail.com>
2024-04-07 21:10:57 -0400

    changes made to logger.py file

---
+++ spotify/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y_%H%M%S')}.log"
+LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
+print("inside logger:" + f"{LOG_FILE_DIR}")
+print("inside logger:" + f"{LOG_FILE_NAME}")
+
+# create log file folder if it does not exists
+os.makedirs(LOG_FILE_DIR,exist_ok= True)
+
+# log file
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s]%(lineno)d %(name)s - %(levelname)s - %(message)s", 
+    level= logging.INFO,
+
+)
+
+
+
